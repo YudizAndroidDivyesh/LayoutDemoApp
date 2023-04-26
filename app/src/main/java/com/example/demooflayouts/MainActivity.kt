b8326d70ac57946630815f0f8ae2a9ae4284cd57
yudizsolutionsltd <divyesh.valera@yudiz.com>
2023-04-26 15:22:25 +0530

    Registration and Profile Screen

--- app/src/main/java/com/example/demooflayouts/MainActivity.kt
+++ app/src/main/java/com/example/demooflayouts/MainActivity.kt
@@ -5,13 +5,8 @@ import android.os.Bundle
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
     }
 }
