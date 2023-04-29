acae69052f1c649ed6e514f9feeb322a6fa16bc0
DivyeshValera <valeradivyesh@gmail.com>
2023-04-29 20:26:23 +0530

    Session 3 - Drawable , SnackBar and Floating Action Button

---
+++ app/src/main/java/com/example/demooflayouts/MetarialUI.kt
@@ -0,0 +1,38 @@
+package com.example.demooflayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+
+class MetarialUI : AppCompatActivity() {
+
+    lateinit var normalFAB : FloatingActionButton
+    lateinit var extendFAB : ExtendedFloatingActionButton
+    lateinit var bgColor : ConstraintLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_metarial_ui)
+
+        normalFAB = findViewById(R.id.fbBtn)
+        extendFAB = findViewById(R.id.extendFb)
+        bgColor = findViewById(R.id.fabLayout)
+
+        normalFAB.setOnClickListener {
+            Snackbar.make(normalFAB,"Added Successfully",Snackbar.LENGTH_SHORT).show()
+        }
+
+        extendFAB.setOnClickListener {
+            Snackbar.make(extendFAB, "Extended Successfully", Snackbar.LENGTH_LONG)
+                .setAction("ChangeColor") {
+                    bgColor.setBackgroundColor(resources.getColor(R.color.colorSecondary))
+                }.setBackgroundTint(resources.getColor(R.color.snackBarColor))
+                .setActionTextColor(resources.getColor(R.color.teal_200))
+                .show()
+        }
+
+    }
+}
