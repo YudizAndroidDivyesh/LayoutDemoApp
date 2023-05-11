36b8ad4e9645f01a7ba6aabd296c0585d1b269c4
yudizsolutionsltd <divyesh.valera@yudiz.com>
2023-05-11 18:40:15 +0530

    LAYOUT PRACTICE

---
+++ app/src/main/java/com/example/demooflayouts/AllLayouts/ContrainsLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.example.demooflayouts.AllLayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.demooflayouts.R
+
+class ContrainsLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contrains_layout)
+    }
+}
